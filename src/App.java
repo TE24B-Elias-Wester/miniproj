8a558bfdec63009b062998f18cf9d49916e4421f
TE24B-Elias-Wester <elias.wester@elev.ga.ntig.se>
2026-09-21 11:08:49 +0200

    092126

--- src/App.java
+++ src/App.java
@@ -1,15 +1,17 @@
 
 void main() {
     int val = 0;
-    int[][] seats = { {01, 2, 3, 4},
-                      {5, 6, 7, 8},
-                      {9, 10, 11, 12},
-                      {13, 14, 15, 16},
-                      {17, 18, 19, 20} };
+    // int[][] seats = { {01, 2, 3, 4},
+    //                   {5, 6, 7, 8},
+    //                   {9, 10, 11, 12},
+    //                   {13, 14, 15, 16},
+    //                   {17, 18, 19, 20} };
+    // int[] seats = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+    String[] seats = {"01", "02", "030000", "04", "05", "06", "07", "080000", "09", "10", "11", "120000", "13", "14", "15", "16", "17", "18", "19", "20"};
     
     while (val != 4) {
         val = 0;
-        IO.println("1: Lägg till bokning\n2: Visa innehåll\n3: Beräkna vinst\n4: Avsluta");
+        IO.println("\n1: Lägg till bokning\n2: Visa innehåll\n3: Beräkna vinst\n4: Avsluta");
         try {
             val = Integer.parseInt(IO.readln("Välj: "));
         } catch (Exception e) {
@@ -18,11 +20,20 @@ void main() {
             case 1 -> {
 
                 int seat = chooseSeat(seats);
-                enterIdentification(seat, seats);
+                String ID = enterIdentification(seats);
+                seats[seat] = ID;
 
             }
-            case 2 -> {}
-            case 3 -> {}
+            case 2 -> {
+
+                printSeats(seats);
+
+            }
+            case 3 -> {
+
+                IO.println(calculateEarnings(seats));
+
+            }
             case 4 -> {break;}
             default -> {
                 IO.println("Ogiltigt val, försök igen");
@@ -35,19 +46,29 @@ void main() {
     
 }
 
-double calculateEarnings(int[][] seats) {
+double calculateEarnings(String[] seats) {
     double profit = 0;
-    for (int row = 0; row < seats.length; row++) {
-        for (int col = 0; col < seats[row].length; col++) {
-            if (seats[row][col] != 0) {
-                profit = 299.90 + profit;
+    int people = 0;
+    for (int i = 0; i < seats.length; i++) {
+        boolean occupied = false;
+        try {
+            int foo = Integer.parseInt(seats[i]);
+            if (foo > 20) {
+                occupied = true;
             }
+        } catch (Exception e) {
+            occupied = true;
+        }
+        if (occupied) {
+            people++;
         }
     }
+    profit = people * 299.90;
+    profit = (double) Math.round(profit * 100) / 100;
     return profit;
 }
 
-void enterIdentification(int seatsLoc, int[][] seats) {
+String enterIdentification(String[] seats) {
     int tmp = 0;
     String s_date = "";
 
@@ -55,49 +76,25 @@ void enterIdentification(int seatsLoc, int[][] seats) {
         s_date = IO.readln("Enter your birth date (YYMMDD): ");
         try {
             tmp = Integer.parseInt(s_date);
+            if (tmp > 999999 || tmp < 000101) {
+                IO.println("Incorrect input, try again");
+            }
+            else break;
         } catch (Exception e) {
             IO.println("Incorrect input, try again");
         }
-        tmp =+ 1000000;
-        if (tmp > 1999999 || tmp < 1000101) {
-            IO.println("Incorrect input, try again");
-        }
-        else break;
     }
-    switch (seatsLoc) {
-        case 1 -> {seats[0][0] = tmp;}
-        case 2 -> {seats[0][1] = tmp;}
-        case 3 -> {seats[0][2] = tmp;}
-        case 4 -> {seats[0][3] = tmp;}
-        case 5 -> {seats[1][0] = tmp;}
-        case 6 -> {seats[1][1] = tmp;}
-        case 7 -> {seats[1][2] = tmp;}
-        case 8 -> {seats[1][3] = tmp;}
-        case 9 -> {seats[2][0] = tmp;}
-        case 10 -> {seats[2][1] = tmp;}
-        case 11 -> {seats[2][2] = tmp;}
-        case 12 -> {seats[2][3] = tmp;}
-        case 13 -> {seats[3][0] = tmp;}
-        case 14 -> {seats[3][1] = tmp;}
-        case 15 -> {seats[3][2] = tmp;}
-        case 16 -> {seats[3][3] = tmp;}
-        case 17 -> {seats[4][0] = tmp;}
-        case 18 -> {seats[4][1] = tmp;}
-        case 19 -> {seats[4][2] = tmp;}
-        case 20 -> {seats[4][3] = tmp;}
-    
-        default -> {}
-    }
+    return s_date;
 }
 
-int chooseSeat(int[][] seats) {
+int chooseSeat(String[] seats) {
 
     int foo = 0;
     String s_foo = "";
 
     while (true) {
-        printSeats(seats);
-        s_foo = IO.readln("Please select a seat: ");
+        printAvailableSeats(seats);
+        s_foo = IO.readln("\nPlease select a seat: ");
         try {
             foo = Integer.parseInt(s_foo);
             break;
@@ -105,27 +102,63 @@ int chooseSeat(int[][] seats) {
             IO.println("Incorrect input, try again");
         }
     }
-    return foo;
+    return foo - 1;
 }
 
-void printSeats(int[][] seats) {
-    for (int row = 0; row < seats.length; row++) {
-        for (int col = 0; col < seats[row].length; col++) {
-            IO.print("|");
-            if (seats[row][col] > 20) {
-                IO.print(" X");
-            }
-            else {
-                if (seats[row][col] < 10) {
-                    IO.print(" ");
-                }
-                IO.print(seats[row][col]);
-            }
-            if ((col+1) == (seats[col].length/2)) {
-                IO.print("|  ");
-            }
+void printSeats(String[] seats) {
+    for (int i = 0; i < seats.length; i++) {
+        if (i % 2 == 0 && i % 4 != 0) {
+            IO.print("  |");
         }
-        IO.print("|\n");
+        if (i % 4 == 0) {
+            IO.print("\n|" + seats[i] + "|");
+        } else {
+            IO.print(seats[i] + "|");
+        }
+    }
+    // for (int row = 0; row < seats.length; row++) {
+        //     for (int col = 0; col < seats[row].length; col++) {
+            //         IO.print("|");
+            //         if (seats[row][col] > 20) {
+                //             IO.print(" X");
+                //         }
+    //         else {
+    //             if (seats[row][col] < 10) {
+        //                 IO.print(" ");
+        //             }
+        //             IO.print(seats[row][col]);
+    //         }
+    //         if ((col+1) == (seats[col].length/2)) {
+        //             IO.print("|  ");
+    //         }
+    //     }
+    //     IO.print("|\n");
+    // }
+}
+
+void printAvailableSeats(String[] seats) {
+    for (int i = 0; i < seats.length; i++) {
+        boolean occupied = false;
+        try {
+            int foo = Integer.parseInt(seats[i]);
+            if (foo > 20) {
+                occupied = true;
+            }
+        } catch (Exception e) {
+            occupied = true;
+        }
+        if (i % 2 == 0 && i % 4 != 0) {
+            IO.print("  |");
+        }
+        if (i % 4 == 0) {
+            if (occupied) {
+                IO.print("\n| x|");
+            } else {IO.print("\n|" + seats[i] + "|");}
+        } else {
+            if (occupied) {
+                IO.print(" x|");
+            } else {IO.print(seats[i] + "|");}
+        }
     }
 }
 //    1  2      3  4
